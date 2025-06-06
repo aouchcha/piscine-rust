21e9491c190240efe5df563bb3de0c49fef0185f
aouchcha <ouchchatea@gmail.com>
2025-06-06 19:29:53 +0100

    string_literals

--- string_literals/src/lib.rs
+++ string_literals/src/lib.rs
@@ -3,8 +3,8 @@ pub fn is_empty(v: &str) -> bool {
 }
 
 pub fn is_ascii(v: &str) -> bool {
-    for i in v.chars() {
-        if (i.to_ascii_lowercase() as u8 )< 32 || (i.to_ascii_lowercase() as u8) > 128 {
+    for c in v.chars() {
+        if (c as i8) < 32 || (c as i8) > 126 {
             return false;
         }
     }
@@ -12,27 +12,24 @@ pub fn is_ascii(v: &str) -> bool {
 }
 
 pub fn contains(v: &str, pat: &str) -> bool {
-    let mut index = 0;
-    for _i in v.chars() {
-        if v[index..pat.len()] == *pat {
+    let size = pat.len();
+    for (i, _) in v.chars().enumerate() {
+        if i + size < v.len() && &v[i..i + size] == pat {
             return true;
         }
-        index += 1;
     }
     false
 }
 
 pub fn split_at(v: &str, index: usize) -> (&str, &str) {
-    (&v[..index], &v[index..])
+    (&v[0..index], &v[index..])
 }
 
 pub fn find(v: &str, pat: char) -> usize {
-    let mut index: usize = 0;
-    for i in v.chars() {
-        if pat == i {
-            break
+    for (i, c) in v.chars().enumerate() {
+        if c == pat {
+            return i;
         }
-        index += 1
     }
-    index
+    return 0 as usize
 }
