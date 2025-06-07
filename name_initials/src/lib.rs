bbd1b9eeb02a9f708dd3158daa6149eed9ffa02f
aouchcha <ouchchatea@gmail.com>
2025-06-07 17:20:15 +0100

    name_initials

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -1,18 +1,23 @@
 pub fn initials(names: Vec<&str>) -> Vec<String> {
-    let mut test : Vec<String> = vec![];
-    for i in names {
-       test.push(split(i));
-    }
-    test
-}
-pub fn split(text :&str) -> String {
-    let mut temp = String::new();
-    for (i, item )in text.chars().enumerate() {
-        if i == 0 {
-            temp.push_str(&(item.to_string() + ". "));
-        }else if item == ' ' {
-            temp.push_str(&(text[i+1..i+2].to_string() + "."));
+    let mut result : Vec<String> = Vec::new();
+    let mut holder = String::new();
+    let mut index = 0;
+    for name in names {
+        for c in name.chars() {
+            if c == ' ' {
+                index = 0;
+                continue;
+            }
+            if index == 0 && holder.len() == 0{
+                holder.push_str(&format!("{}. ",c));
+            }else if index == 0 {
+                holder.push_str(&format!("{}.",c));
+            }
+            index += 1;
         }
+        index = 0;
+        result.push(holder);
+        holder = String::new()
     }
-    temp
+    result
 }
