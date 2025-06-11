d2766db4cf3ab401e940d5706dd7f71e2d0752ca
aouchcha <ouchchatea@gmail.com>
2025-06-11 22:22:54 +0100

    commit

---
+++ panic/src/main.rs
@@ -0,0 +1,28 @@
+use panic::*;
+use std::fs::{self, File};
+
+fn main() {
+    let filename = "created.txt";
+    File::create(filename).unwrap();
+
+    println!("{:?}", open_file(filename));
+
+    fs::remove_file(filename).unwrap();
+
+    // this should panic!
+    open_file(filename);
+}
+
+#[test]
+#[should_panic(expected = "No such file or directory")]
+fn test_opening() {
+    open_file("file.txt");
+}
+
+#[test]
+fn test_opening_existing() {
+    let filename = "created.txt";
+    File::create(filename).unwrap();
+    open_file(filename);
+    fs::remove_file(filename).unwrap();
+}
