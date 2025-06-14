c94717e80085d6a7d847cbab9f2e03a3e3b44363
aouchcha <ouchchatea@gmail.com>
2025-06-14 22:08:04 +0100

    stars

--- stars/src/lib.rs
+++ stars/src/lib.rs
@@ -1,8 +1,7 @@
 pub fn stars(n: u32) -> String {
-    let mut r : String = String::new();
-    let end = (2 as i32).pow(n);
-    for _ in 0..end {
-        r.push_str("*")
+    let mut res = String::new();
+    for _ in 0..2_i32.pow(n) {
+        res.push_str("*")
     }
-    r
+    res
 }
