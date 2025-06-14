d6e3fd46a7f72cc474224746534271f69aa3c98e
aouchcha <ouchchatea@gmail.com>
2025-06-14 22:31:24 +0100

    searching

--- searching/src/lib.rs
+++ searching/src/lib.rs
@@ -1,20 +1,9 @@
 pub fn search(array: &[i32], key: i32) -> Option<usize> {
-    let mut i = 0;
-    let mut j = i+1;
-    while i < array.len() {
-        while j < array.len()-1 {
-            if array[i] == array[j] {
-                return None
-            }
-            j+=1;
+    for i in 0..array.len() {
+        if array[i] == key {
+            return Some(i)
         }
-        i+=1;
     }
-
-   for num in 0..array.len() {
-       if array[num] == key {
-        return Some(num)
-       }
-    }
-    None
+    return None
 }
+
