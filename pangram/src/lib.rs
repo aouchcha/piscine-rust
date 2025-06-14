64243c4e7d7272a9a002973b81bde429696555b8
aouchcha <ouchchatea@gmail.com>
2025-06-14 22:52:03 +0100

    pangram

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -2,12 +2,9 @@ use std::collections::HashMap;
 
 pub fn is_pangram(s: &str) -> bool {
     let mut map = HashMap::new();
-    let ss = s.to_lowercase();
-    for c in ss.chars() {
-        if c>= 'a' && c <='z' {
-            println!("{c}");
-            let count = map.entry(c).or_insert(1);
-            *count += 1;
+    for c in s.to_lowercase().chars() {
+        if c.is_ascii_alphabetic() {
+            let _count = map.entry(c).or_insert(1);
         }
     }
     map.len() == 26
