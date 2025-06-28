6967eaf2968a054a0255f330e724bdd746aa363a
Ayamne <ouchchateaymane@gmail.cpm>
2025-06-28 16:57:23 +0100

    easy_traits

---
+++ easy_traits/src/main.rs
@@ -0,0 +1,23 @@
+use easy_traits::*;
+
+fn main() {
+    let mut str_aux = StringValue {
+        value: String::from("hello"),
+    };
+
+    println!("Before append: {}", str_aux.value);
+
+    str_aux.append_str(String::from(" there!"));
+    println!("After append: {}", str_aux.value);
+
+    str_aux.remove_punctuation_marks();
+    println!("After removing punctuation: {}", str_aux.value);
+}
+
+/*
+    $ cargo run
+        Before append: hello
+        After append: hello there!
+        After removing punctuation: hello there
+    $
+*/
