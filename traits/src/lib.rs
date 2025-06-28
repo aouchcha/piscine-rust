5666709c035572196e521f8673777e554e148c1c
Ayamne <ouchchateaymane@gmail.cpm>
2025-06-28 17:46:09 +0100

    traits

--- traits/src/lib.rs
+++ traits/src/lib.rs
@@ -38,8 +38,7 @@ impl Food for Fruit {
 
 impl Food for Meat {
     fn gives(&self) -> f64 {
-        let protein = self.weight_in_kg % self.fat_content;
-        ( protein * 4.0 ) + ( self.fat_content * 9.0 )
+        (self.weight_in_kg * (1.0 - self.fat_content) * 4.0 ) + ( self.fat_content * self.weight_in_kg * 9.0 )
     }
 }
 
